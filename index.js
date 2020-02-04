a7d5cb290f1491f770a091aa40a327069af41a40
Akaki <akakikoridze@yahoo.com>
2020-02-04 12:24:19 +0400

    updated design

--- index.js
+++ index.js
@@ -22,8 +22,20 @@ function scrollit(arg) {
             b.classList.remove("active")
         }
 
+
+
     let c = document.querySelector(`.${arg}`)
     c.classList.add('active')
-    
-    
+}
+
+
+if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+    var typed = new Typed('.text-slider', {
+        strings: typed_strings.split(','),
+        typeSpeed: 80,
+        loop: true,
+        backDelay: 1100,
+        backSpeed: 30
+    });
 }
